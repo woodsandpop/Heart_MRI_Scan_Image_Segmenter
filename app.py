b50bb27e37cf4227b7ca054d5778f2f8952aefd2
woodsandpop <132575779+woodsandpop@users.noreply.github.com>
2023-07-31 20:39:28 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import torch
+import detect
+from PIL import Image
+from io import *
+import glob
+from datetime import datetime
+import os
+import wget
+import time
+
+def imageInput(src):
+    if src == 'Upload your own Image':
+        image_file = st.file_uploader("Upload a 3D Heart MRI Image", type=['png', 'jpeg', 'jpg'])
+        col1, col2 = st.columns(2)
+        if image_file is not None:
+            img = Image.open(image_file)
+            with col1:
+                st.image(img, caption='Uploaded 3D Heart MRI Image', use_column_width=True)
+            ts = datetime.timestamp(datetime.now())
+            imgpath = os.path.join('data/uploads', str(ts)+image_file.name)
+            outputpath = os.path.join('data/outputs', os.path.basename(imgpath))
+            with open(imgpath, mode="wb") as f:
+                f.write(image_file.getbuffer())
+
+            #call Model prediction--
+            model = torch.hub.load('ultralytics/yolov5', 'custom', path='heart_1st/weights/best.pt', force_reload=True)  
+            #model.cuda() if device == 'cuda' else model.cpu()
+            pred = model(imgpath)
+            pred.render()  # render bbox in image
+            for im in pred.ims:
+                im_base64 = Image.fromarray(im)
+                im_base64.save(outputpath)
+
+            #--Display predicton
+            img_ = Image.open(outputpath)
+            with col2:
+                st.image(img_, caption='Predicted Heart Segmentation', use_column_width=True)
+
+    elif src == 'From sample Images': 
+        # Image selector slider
+        imgpath = glob.glob('data/images/test/*')
+        imgsel = st.slider('Select random images from test set.', min_value=1, max_value=len(imgpath), step=1) 
+        image_file = imgpath[imgsel-1]
+        submit = st.button("Predict the Heart Segmentation")
+        col1, col2 = st.columns(2)
+        with col1:
+            img = Image.open(image_file)
+            st.image(img, caption='Selected Image', use_column_width='always')
+        with col2:            
+            if image_file is not None and submit:
+                #call Model prediction--
+                model = torch.hub.load('ultralytics/yolov5','custom', path= 'heart_1st/weights/best.pt', force_reload=True) 
+                pred = model(image_file)
+                pred.render()  # render bbox in image
+                for im in pred.ims:
+                    im_base64 = Image.fromarray(im)
+                    im_base64.save(os.path.join('data/outputs', os.path.basename(image_file)))
+                #--Display predicton
+                    img_ = Image.open(os.path.join('data/outputs', os.path.basename(image_file)))
+                    st.image(img_, caption='Predicted Heart Segmentation')
+
+def main():
+    
+    st.image("logo.jpg", width = 500)
+    st.title("3D Heart MRI Image Segmentation")
+    st.header("👈🏽 Select the Image Source options")
+    st.sidebar.title('⚙️Options')
+    src = st.sidebar.radio("Select input source.", ['From sample Images', 'Upload your own Image'])
+    imageInput(src)
+   
+if __name__ == '__main__':    
+    main()
+    
+#@st.cache_data
+def loadModel():
+    start_dl = time.time()
+    model_file = wget.download('https://archive.org/download/yoloTrained/yoloTrained.pt', out="models/")
+    finished_dl = time.time()
+    print(f"Model Downloaded, ETA:{finished_dl-start_dl}")
+    
+loadModel()

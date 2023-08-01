5b6422a44d12e80c870a4bcb67f56771dcbb1eff
woodsandpop <132575779+woodsandpop@users.noreply.github.com>
2023-08-01 14:31:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import os
 import wget
 import time
 
+
 def imageInput(src):
     if src == 'Upload your own Image':
         image_file = st.file_uploader("Upload a 3D Heart MRI Image", type=['png', 'jpeg', 'jpg'])
